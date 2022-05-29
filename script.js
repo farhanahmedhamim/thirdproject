f9bf9f4c69231713b304d9015cded7814ddd5113
Farhan Hamim <farhanahmedhamim33@gmail.com>
2022-05-29 19:11:43 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var menu = document.querySelector(".menu")
+var bar = document.querySelector(".fa-bars")
+
+
+function toggleclass() {
+    menu.classList.toggle("active")
+    bar.classList.toggle("active")
+}
